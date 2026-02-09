4eeb6f692ca2835ea5cbe377a45497afdf120367
Lilinta <abcxyz22334@gmail.com>
2026-02-09 22:22:59 +0700

    Change generator and  testcases

--- generator.py
+++ generator.py
@@ -6,28 +6,28 @@ if __name__ == '__main__':
     with open(os.path.join(os.path.dirname(__file__), "testcases/test1.txt"), "w") as f:
         A = [0.0]*length
         for i in range(length):
-            A[i] = random.uniform(1, 1000000000)
+            A[i] = round(random.uniform(1, 1000000000), 5)
         A.sort()
         for i in range(length):
             f.write(str(A[i]))
             if i != length: f.write(" ")
             else: f.write("\n")
     with open(os.path.join(os.path.dirname(__file__), "testcases/test2.txt"), "w") as f:
-        A = [0]*length
+        A = [0.0]*length
         for i in range(length):
-            A[i] = random.randint(1, 1000000000)
+            A[i] = round(random.uniform(1, 1000000000), 5)
         A.sort(reverse=True)
         for i in range(length):
             f.write(str(A[i]))
             if i != length: f.write(" ")
             else: f.write("\n")
-    for num in range(3, 7):
+    for num in range(3, 6):
         with open(os.path.join(os.path.dirname(__file__), "testcases/test" + str(num) + ".txt"), "w") as f:
             for i in range(length):
-                f.write(str(random.uniform(1, 1000000000)))
+                f.write(str(round(random.uniform(1, 1000000000), 5)))
                 if i != length: f.write(" ")
                 else: f.write("\n")
-    for num in range(7, 11):
+    for num in range(6, 11):
         with open(os.path.join(os.path.dirname(__file__), "testcases/test" + str(num) + ".txt"), "w") as f:
             for i in range(length):
                 f.write(str(random.randint(1, 1000000000)))
