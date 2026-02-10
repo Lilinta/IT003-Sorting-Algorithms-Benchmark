7faf62886468d6dab494efaa7081abdaca405fe3
Lilinta <abcxyz22334@gmail.com>
2026-02-10 20:08:39 +0700

    Improve quicksort algorithm

--- quicksort.py
+++ quicksort.py
@@ -1,25 +1,25 @@
 import numpy as np
 from random import randint
 
-def quicksort(A, l, r):
-    if l >= r:
-        return
-    pivot = randint(l, r)
-    A[l], A[pivot] = A[pivot], A[l]
-    j = l+1
-    for i in range(l+1, r+1):
-        if A[i] < A[l]:
-            A[i], A[j] = A[j], A[i]
-            j += 1
-    A[l], A[j-1] = A[j-1], A[l]
-    quicksort(A, l, j-2)
-    while j <= r and A[j] == A[j - 1]:
-        j += 1
-    quicksort(A, j, r)
-    
+def quicksort(A):
+    if (len(A) <= 1):
+        return A
+    B = []
+    C = []
+    D = []
+    pivot = randint(0, len(A)-1)
+    for i in range(0, len(A)):
+        if A[i] < A[pivot]:
+            B.append(A[i])
+        elif A[i] == A[pivot]:
+            C.append(A[i])
+        else:
+            D.append(A[i])
+    return quicksort(B) + C + quicksort(D)
+
 
 if __name__ == "__main__":
     A = np.array(list(map(int, input().split())))
-    quicksort(A, 0, len(A)-1)
+    A = quicksort(A)
     for i in range(0, len(A)):
         print(A[i], end=" ")
