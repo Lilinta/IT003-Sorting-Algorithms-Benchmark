8ddc6bdb9f7848bcb04ee5f7f2fc1e57acbd59c5
Lilinta <abcxyz22334@gmail.com>
2026-02-10 20:36:34 +0700

    Update and rerun create_chart.py

--- create_chart.py
+++ create_chart.py
@@ -4,13 +4,13 @@ from openpyxl import load_workbook
 
 wb = load_workbook(filename='result.xlsx')
 ws = wb.active
-tests = ["test" + str(i) for i in range(1, 6)]
+tests = ["test" + str(i) for i in range(6, 11)]
 result = {}
 
 for c in range(2, 7):
     algo = ws.cell(row=1, column=c).value
     result[algo] = []
-    for r in range(2, 7):
+    for r in range(7, 12):
         result[algo].append(ws.cell(row=r, column=c).value)
 
 x = np.arange(len(tests))  # the label locations
@@ -31,7 +31,7 @@ ax.set_ylabel('Thời gian thực hiện (ms)')
 ax.set_title('Kết quả thử nghiệm bộ dữ liệu')
 ax.set_xticks(x + width*2, tests)
 ax.legend(loc='right', bbox_to_anchor=(1.05, 0.5), ncol=1)
-ax.set_ylim(0, 8200)
+ax.set_ylim(0, 30000)
 
 plt.show()
 
